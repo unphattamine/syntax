9bbc389dea60b448d22beb27a01b7a469ba01ed9
stewe-olsi <stefan.olsavsky@hotmail.com>
2024-07-18 14:50:21 +0200

    Added loop to keep chat more interactional

--- llamaindex-sqlReader/sqlQuery.py
+++ llamaindex-sqlReader/sqlQuery.py
@@ -24,26 +24,10 @@ db_name = "llama_test"
 db_port = "3314"
 connection_uri = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
-
 # Function to get the question from the user
 def get_question():
     question = input("Please ask your question here: ")
     return question
-
-
-# Trigger question
-user_question = get_question()
-
-# Create SQLAlchemy engine and inspect table names
-print("\nCreating DB connection...")
-engine = create_engine(connection_uri)
-sql_database = SQLDatabase(engine)
-inspector = inspect(engine)
-table_names = inspector.get_table_names()
-print("Please wait, I'm gettin' results from following tables", table_names, "from", db_name, "database...\n")
-# Configure LLM settings
-Settings.llm = OpenAI(temperature=0.5, model="gpt-3.5-turbo-16k")
-
 
 # Set query function
 def chat_to_sql(question: str | list[str], tables: list[str] | None = None, synthesize_response: bool = True):
@@ -66,12 +50,36 @@ def chat_to_sql(question: str | list[str], tables: list[str] | None = None, synt
         sql = f"ERROR: {str(ex)}"
         duration = None
 
+    # Prepare output formatting
     print(Fore.GREEN + "Question:\t", question)
     print(Fore.RED + "Response:\t", response_md)
-    print(Fore.BLUE + "\nSQL Query I ran:\n", sql)
+    print(Fore.RESET + "\nSQL Query I ran:\n", Fore.BLUE, sql)
     if duration is not None:
-        print(Fore.WHITE + f"Query took me: {duration:.2f} seconds")
+        print(Fore.RESET + f"\nQuery took me: {duration:.2f} seconds\n")
 
+# Starting the show, connection to database
+try:
+    # Create SQLAlchemy engine and inspect table names
+    print("\nWelcome to Syntax Q&A dbSQL chat.")
+    print("\nCreating DB connection...")
+    engine = create_engine(connection_uri)
+    sql_database = SQLDatabase(engine)
+    inspector = inspect(engine)
+    table_names = inspector.get_table_names()
+    print("Please wait, I'm loading tables", table_names, "of", db_name, "database...\n")
+    # Configure LLM settings
+    Settings.llm = OpenAI(temperature=0.5, model="gpt-3.5-turbo-16k")
+except Exception as ex:
+    errConn = "I was not able to connect to MySQL database"
+    print(errConn)
 
-# Define query against DB
-chat_to_sql(user_question, table_names)
+# Main loop
+while True:
+
+    # Trigger question
+    user_question = get_question()
+    if user_question == "exit":
+        break
+    else:
+        # Define query against DB
+        chat_to_sql(user_question, table_names)
