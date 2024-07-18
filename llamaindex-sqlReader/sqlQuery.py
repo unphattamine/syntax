135aabc6224cd0a8bbb8c4edebb89806886cbf84
stewe-olsi <stefan.olsavsky@hotmail.com>
2024-07-18 11:14:30 +0200

    Adding comment

--- llamaindex-sqlReader/sqlQuery.py
+++ llamaindex-sqlReader/sqlQuery.py
@@ -23,22 +23,34 @@ db_name = "llama_test"
 db_port = "3314"
 connection_uri = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
+
+# Function to get the question from the user
+def get_question():
+    question = input("Please ask your question here: ")
+    return question
+
+
+# Trigger question
+user_question = get_question()
+
 # Create SQLAlchemy engine and inspect table names
+print("\nCreating DB connection...")
 engine = create_engine(connection_uri)
 sql_database = SQLDatabase(engine)
 inspector = inspect(engine)
 table_names = inspector.get_table_names()
-
+print("Please wait, I'm gettin' results from following tables", table_names, "from", db_name, "database...\n")
 # Configure LLM settings
 Settings.llm = OpenAI(temperature=0.5, model="gpt-3.5-turbo-16k")
 
-#Set query function
+
+# Set query function
 def chat_to_sql(question: str | list[str], tables: list[str] | None = None, synthesize_response: bool = True):
     query_engine = NLSQLTableQueryEngine(
         sql_database=sql_database,
         tables=tables,
         synthesize_response=synthesize_response,
-        settings=Settings,
+        settings=Settings
     )
 
     try:
@@ -53,13 +65,12 @@ def chat_to_sql(question: str | list[str], tables: list[str] | None = None, synt
         sql = f"ERROR: {str(ex)}"
         duration = None
 
-    print(Fore.GREEN + "Question:", question)
-    print(Fore.RED + "Response:", response_md)
-    print(Fore.BLUE + "SQL Query I ran:", sql)
+    print(Fore.GREEN + "Question:\t", question)
+    print(Fore.RED + "Response:\t", response_md)
+    print(Fore.BLUE + "\nSQL Query I ran:\n", sql)
     if duration is not None:
         print(Fore.WHITE + f"Query took me: {duration:.2f} seconds")
 
 
-#Define query against DB
-
-chat_to_sql("Which employee except AI consultans has the bigger earnings and what's his position? Do you know what his position means?", table_names)
+# Define query against DB
+chat_to_sql(user_question, table_names)
