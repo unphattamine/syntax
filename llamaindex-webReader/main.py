03505ca5e234fbdb5f4f04dc8efd588c653fc00c
Samuel Imrich <SImrich@bell-integration.com>
2024-07-18 09:07:47 +0200

    update of main

--- llamaindex-webReader/main.py
+++ llamaindex-webReader/main.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 from llama_index.readers.web import SimpleWebPageReader
 from llama_index.core import VectorStoreIndex
 
+#define input query to ask
 def query():
     query = input("Please ask your question here: ")
     #query = "Tell me which services are offered and what is the contact email address or phone?"
     return query
+
+#main function to read webpage and utilize query engine for response
 def main(url:str)-> None:
     try:
         documents = SimpleWebPageReader(html_to_text=True).load_data(urls=[url])
